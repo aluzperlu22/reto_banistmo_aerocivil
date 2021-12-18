d00bb36c3c9204cd8a235e3c8e41386caa3981da
aluzperlu22 <adaluzperez135@gmail.com>
2021-12-17 21:34:47 -0500

    Correccion de variables

--- src/main/java/co/com/testtechnical/interactions/SegundaVentana.java
+++ src/main/java/co/com/testtechnical/interactions/SegundaVentana.java
@@ -10,26 +10,26 @@ import org.openqa.selenium.WebDriver;
 import java.util.Set;
 
 public class SegundaVentana implements Interaction {
-    private String titulo;
-    public SegundaVentana(String titulo) {
-        this.titulo = titulo;
+    private String encabezado;
+    public SegundaVentana(String encabezado) {
+        this.encabezado = encabezado;
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
         WebDriver driver=BrowseTheWeb.as(actor).getDriver();
-        Set<String> windowslist;
-        windowslist=driver.getWindowHandles();
-        for(String ventana:windowslist){
-            driver.switchTo().window(ventana);
-            if(driver.getTitle().contains(titulo)){
+        Set<String> listadepestanias;
+        listadepestanias=driver.getWindowHandles();
+        for(String pestania:listadepestanias){
+            driver.switchTo().window(pestania);
+            if(driver.getTitle().contains(encabezado)){
                 break;
             }
         }
           }
 
-    public static SegundaVentana a(String titulo){
+    public static SegundaVentana a(String encabezado){
 
-        return Tasks.instrumented(SegundaVentana.class,titulo);
+        return Tasks.instrumented(SegundaVentana.class,encabezado);
     }
 }
